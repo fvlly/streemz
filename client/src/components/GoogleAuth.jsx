b3e24608f7a322c8c323b8b78f03667de32dbe8e
Fvlly <luki0000001@gmail.com>
2022-07-16 10:55:43 +0100

    redux refactor

--- client/src/components/GoogleAuth.jsx
+++ client/src/components/GoogleAuth.jsx
@@ -1,8 +1,9 @@
 import React, { useRef, useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
-
-const GoogleAuth = () => {
-  const [signIn, setSignIn] = useState(null);
+import { connect } from "react-redux";
+import { signIn, signOut } from "../actions";
+const GoogleAuth = (props) => {
+  //   const [signIn, setSignIn] = useState(null);
   const auth = useRef("");
 
   useEffect(() => {
@@ -17,45 +18,53 @@ const GoogleAuth = () => {
         .then(() => {
           //init  returns a promsie after initialization
           auth.current = window.gapi.auth2.getAuthInstance(); // ref to auth instance to signin/out
-          setSignIn(auth.current.isSignedIn.get());
+          //   setSignIn(auth.current.isSignedIn.get());
+          onAuthChange(auth.current.isSignedIn.get());
           auth.current.isSignedIn.listen(onAuthChange);
         });
     });
   }, []);
 
   const renderAuthButton = () => {
-    if (signIn === null) {
+    if (props.isSignedIn === null) {
       return null;
-    } else if (signIn) {
+    } else if (props.isSignedIn) {
       return (
-        <Button onClick={()=>onSignOut(auth.current)} variant="warning">
+        <Button onClick={() => onSignOut()} variant="warning">
           <i className="bi bi-google"></i>
           Sign Out
         </Button>
       );
     } else {
       return (
-        <Button onClick={()=>onSignIn(auth.current)} variant="warning">
+        <Button onClick={() => onSignIn()} variant="warning">
           <i className="bi bi-google"></i>
           Sign In
-          
         </Button>
       );
     }
   };
 
-  const onAuthChange = () => {
-    setSignIn(auth.current.isSignedIn.get());
+  const onAuthChange = (isSignedIn) => {
+    if (isSignedIn) {
+      props.signIn();
+    } else {
+      props.signOut();
+    }
   };
 
-  const onSignIn = (auth) =>{
-    setSignIn(auth.signIn())
-  }
-  const onSignOut = (auth) =>{
-    setSignIn(auth.signOut())
-  }
+  const onSignIn = () => {
+    auth.current.signIn();
+  };
+  const onSignOut = () => {
+    auth.current.signOut();
+  };
 
   return <div className="google-auth">{renderAuthButton()}</div>;
 };
 
-export default GoogleAuth;
+const mapStateToProps = (state) => {
+  return { isSignedIn: state.auth.isSignedIn };
+};
+
+export default connect(mapStateToProps, { signIn, signOut })(GoogleAuth);
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
+import App from "./App";
+import reducers from "./reducers";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const store = createStore(reducers);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
-)
+);
